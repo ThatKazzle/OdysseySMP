5b9c76f02a5f36e0d38094d7b41aac4382c5b169
ItsKazzle <thatkazzle@gmail.com>
2024-07-23 16:18:11 -0500

    Update 1.8.5
    date 1.8.5
    - added a descriptions command, /od descriptions which lets you read the descriptions of any power, the 2nd argument of the command lets you choose a power to read the description of
    
    - added /od powers (not polished, doing that rn) that lets you see if any of the powers are taken, but not by who, I can add that if you want

--- src/main/java/commands/odysseyCommands.java
+++ src/main/java/commands/odysseyCommands.java
@@ -22,16 +22,13 @@ import org.bukkit.util.StringUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class odysseyCommands implements CommandExecutor, TabCompleter, Listener {
     SimpleS5 plugin;
     String usageString = ChatColor.RED + "usage: /odyssey <withdraw|powers>";
 
-    private static final List<String> AUTOFILL_ARGS_1 = Arrays.asList("withdraw", "powers");
+    private static final List<String> AUTOFILL_ARGS_1 = Arrays.asList("withdraw", "description", "powers");
     private static final List<String> AUTOFILL_ARGS_2 = new ArrayList<>();
 
     private NamespacedKey canClickKey;
@@ -54,18 +51,22 @@ public class odysseyCommands implements CommandExecutor, TabCompleter, Listener 
             if (args.length == 1 && args[0].equals("withdraw")) {
                 openGUI(player);
 
-            } else if (args.length == 2 && args[0].equals("powers")) {
+            } else if (args.length == 2 && args[0].equals("description")) {
                 for (String keys : this.plugin.getConfig().getConfigurationSection("defaults.").getKeys(false)) {
                     String value = this.plugin.getConfig().getString("defaults." + keys);
                     AUTOFILL_ARGS_2.add(keys);
 
                     if (args[1].equals(keys)) {
                         sendDescriptionMessage(player, keys);
+                    } else {
+                        player.sendMessage(ChatColor.RED + "Usage: /odyssey description [key of power you want description of]");
                     }
                 }
 
+            } else if (args.length == 1 && args[0].equals("powers")) {
+                sendPowerStatus(player, checkPowerStatus());
             } else {
-                player.sendMessage(usageString);
+                player.sendMessage(ChatColor.RED + "Usage: " + command.getUsage());
             }
 
             return true;
@@ -158,7 +159,90 @@ public class odysseyCommands implements CommandExecutor, TabCompleter, Listener 
     }
 
     private void sendDescriptionMessage(Player player, String key) {
-        player.sendMessage(plugin.getConfig().getStringList("defaults.") + key);
+        player.sendMessage(ChatColor.AQUA + "Description for " + ChatColor.GOLD + ChatColor.BOLD + key + ChatColor.RESET + ChatColor.AQUA + ":");
+        for (String curMessage : plugin.getConfig().getStringList("descriptions." + key)) {
+            player.sendMessage(curMessage);
+        }
         AUTOFILL_ARGS_2.clear();
+    }
+
+    private void sendAllTakenPowersMessage(Player player) {
+        Map<String, Boolean> allPowers = plugin.getAllPowers(plugin.getConfig());
+
+        for (Map.Entry<String, Boolean> entry : allPowers.entrySet()) {
+            plugin.getLogger().info(entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
+    private Map<String, Boolean> checkPowerStatus() {
+        Map<String, Boolean> powerStatus = new HashMap<>();
+
+        // Get the defaults section
+        ConfigurationSection defaultsSection = plugin.getConfig().getConfigurationSection("defaults");
+        if (defaultsSection == null) {
+            return powerStatus; // Return empty map if "defaults" section is not found
+        }
+
+        // Get all default power keys
+        Set<String> defaultPowerKeys = defaultsSection.getKeys(false);
+
+        // Initialize all powers as "not taken"
+        for (String defaultPowerKey : defaultPowerKeys) {
+            powerStatus.put(defaultPowerKey, false);
+        }
+
+        // Get the "players" section
+        ConfigurationSection playersSection = plugin.getConfig().getConfigurationSection("players");
+        if (playersSection == null) {
+            return powerStatus; // Return if "players" section is not found
+        }
+
+        // Loop through each player
+        Set<String> playerKeys = playersSection.getKeys(false);
+        for (String playerKey : playerKeys) {
+            ConfigurationSection powersSection = playersSection.getConfigurationSection(playerKey + ".powers");
+            if (powersSection != null) {
+                // Loop through each power for the player
+                for (String defaultPowerKey : defaultPowerKeys) {
+                    String powerPath = defaultsSection.getString(defaultPowerKey);
+                    if (powersSection.getBoolean(powerPath, false)) {
+                        powerStatus.put(defaultPowerKey, true); // Mark as "taken" if any player has it set to true
+                    }
+                }
+            }
+        }
+
+        return powerStatus;
+    }
+
+//    private void sendPowerStatus(Player player, Map<String, Boolean> powerStatus) {
+//        int maxLength = 0;
+//
+//        // Find the longest key length
+//        for (String key : powerStatus.keySet()) {
+//            if (key.length() > maxLength) {
+//                maxLength = key.length();
+//            }
+//        }
+//
+//        // Send the formatted power status to the player
+//        player.sendMessage("" + ChatColor.LIGHT_PURPLE + ChatColor.BOLD + "List of powers that are taken: ");
+//        player.sendMessage("");
+//
+//        for (Map.Entry<String, Boolean> entry : powerStatus.entrySet()) {
+//            String paddedKey = String.format("%-" + (maxLength - entry.getKey().length()) + "s", entry.getKey());
+//
+//            String status = entry.getValue() ? ChatColor.RED + "Taken" : ChatColor.GREEN + "Not Taken";
+//            player.sendMessage(ChatColor.AQUA + paddedKey + ": " + status);
+//        }
+//    }
+
+    private void sendPowerStatus(Player player, Map<String, Boolean> powerStatus) {
+        player.sendMessage("" + ChatColor.LIGHT_PURPLE + ChatColor.BOLD + "List of powers that are currently taken: ");
+
+        for (Map.Entry<String, Boolean> entry : powerStatus.entrySet()) {
+            String status = entry.getValue() ? ChatColor.RED + "Taken" : ChatColor.GREEN + "Not Taken";
+            player.sendMessage(ChatColor.AQUA + entry.getKey() + ": " + status);
+        }
     }
 }
