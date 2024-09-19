da62f2d04083f6aa81e09dfcb66adfc74ba3491b
ItsKazzle <112206126+ThatKazzle@users.noreply.github.com>
2024-09-19 08:19:46 -0500

    Update 5.2.4.1

--- src/main/java/PowerClasses/CharliPower.java
+++ src/main/java/PowerClasses/CharliPower.java
@@ -76,7 +76,7 @@ public class CharliPower extends ParentPowerClass implements Listener {
             } else if (!isOnCooldown(player.getUniqueId(), cobCooldowns)) {
                 int i = 0;
                 for (Player playerCheck : Bukkit.getOnlinePlayers()) {
-                    if (playerCheck.getLocation().distance(player.getLocation()) < 7 && playerCheck != player && playerCheck.getWorld() == player.getWorld()) {
+                    if (playerCheck != player && playerCheck.getWorld() == player.getWorld() && playerCheck.getLocation().distance(player.getLocation()) < 7) {
                         for (int x = -1; x < 2; x++) {
                             for (int z = -1; z < 2; z++) {
                                 if (playerCheck.getWorld().getBlockAt(playerCheck.getLocation().add(x, 0.5, z)).getType().equals(Material.AIR)) {
