58c477da96d01be9f728c030544566f65bd5badd
ItsKazzle <thatkazzle@gmail.com>
2024-09-27 12:21:09 -0500

    Update 5.7.7
    - hopefully added heavy sniper ability

--- src/main/java/PowerClasses/FortniteOdyssey.java
+++ src/main/java/PowerClasses/FortniteOdyssey.java
@@ -2,15 +2,14 @@ package PowerClasses;
 
 import io.papermc.paper.event.block.BlockBreakBlockEvent;
 import kazzleinc.simples5.SimpleS5;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.block.data.BlockData;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 import org.jetbrains.annotations.NotNull;
@@ -20,6 +19,9 @@ import java.util.*;
 public class FortniteOdyssey extends ParentPowerClass implements Listener {
 
     public final HashMap<UUID, Long> fullBoxCooldowns = new HashMap<>();
+    public final HashMap<UUID, Long> heavySniperCooldowns = new HashMap<>();
+
+    private final HashSet<UUID> shootsPowerBow = new HashSet<>();
 
     public final HashSet<Location> protectedBlocks = new HashSet<>();
 
@@ -37,9 +39,18 @@ public class FortniteOdyssey extends ParentPowerClass implements Listener {
     }
 
     @Override
+    public String getCooldownString(Player player, HashMap<UUID, Long> cooldownMap, String powerName) {
+        return "" + ChatColor.AQUA + powerName + getCooldownTimeLeft(player.getUniqueId(), cooldownMap) + ChatColor.BOLD + ChatColor.GOLD + " | " + ChatColor.RESET + ChatColor.AQUA + "Heavy Sniper: " + (shootsPowerBow.contains(player) ? ChatColor.BLUE + "Activated" : getCooldownTimeLeft(player.getUniqueId(), heavySniperCooldowns));
+    }
+
+    @Override
     public void action(String playerName) {
         Player player = Bukkit.getPlayer(playerName);
-        createAndRestoreCube(player, Material.IRON_BLOCK);
+        if (!player.isSneaking()) {
+            createAndRestoreCube(player, Material.IRON_BLOCK);
+        } else {
+            heavySniperAbility(player);
+        }
     }
 
     public class SavedBlock {
@@ -129,6 +140,20 @@ public class FortniteOdyssey extends ParentPowerClass implements Listener {
         }.runTaskLater(plugin, 200L);  // 200 ticks = 10 seconds
     }
 
+    public void heavySniperAbility(Player player) {
+        if (shootsPowerBow.contains(player.getUniqueId())) {
+            player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 0.5f, 1.f);
+            player.sendMessage(ChatColor.RED + "Already activated.");
+        } else {
+            if (!isOnCooldown(player.getUniqueId(), heavySniperCooldowns)) {
+                player.sendMessage(ChatColor.GOLD + "Your next bow shot does 3x damage.");
+                player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 0.5f, 1.f);
+
+                shootsPowerBow.add(player.getUniqueId());
+            }
+        }
+    }
+
     public void restoreBlocks() {
         // Loop through the saved blocks and restore them
         for (SavedBlock savedBlock : savedBlocks) {
@@ -136,6 +161,20 @@ public class FortniteOdyssey extends ParentPowerClass implements Listener {
             block.setBlockData(savedBlock.getBlockData());  // Restore the original block
         }
         savedBlocks.clear();
+    }
+
+    @EventHandler
+    public void onEntityShootBowEvent(EntityShootBowEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+
+            if (shootsPowerBow.contains(player.getUniqueId())) {
+                event.getProjectile().setVelocity(event.getProjectile().getVelocity().multiply(3));
+
+                shootsPowerBow.remove(player.getUniqueId());
+                setCooldown(player.getUniqueId(), heavySniperCooldowns, (20 * 60));
+            }
+        }
     }
 
     @EventHandler
